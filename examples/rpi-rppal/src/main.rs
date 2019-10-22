875f788f8cbcf178bc58bdb5ec943161df3be9a8
Rumato Estorsky <rumatoest@users.noreply.github.com>
2019-10-22 12:11:47 +0300

    Yes, yes, yes. It hacking works now. Tested on RPi

---
+++ examples/rpi-rppal/src/main.rs
@@ -0,0 +1,210 @@
+extern crate clap;
+
+use clap::{App, Arg, ArgMatches};
+
+use embedded_hal::{
+    blocking::delay::DelayUs,
+    digital::v2::{InputPin, OutputPin},
+};
+use rppal::gpio::{Gpio, IoPin, Mode};
+use spin_sleep;
+use std::{thread, time};
+use void;
+
+use tmledkey_hal_drv as tm;
+
+/**
+ * Raspberry pi does not have open drain pins so we have to emulate it.
+ */
+struct OpenPin {
+    iopin: IoPin,
+    mode: Mode,
+}
+
+impl OpenPin {
+    fn new(mut pin: IoPin) -> OpenPin {
+        pin.set_mode(Mode::Input);
+        OpenPin {
+            iopin: pin,
+            mode: Mode::Input,
+        }
+    }
+
+    fn switch_input(&mut self) {
+        if self.mode != Mode::Input {
+            self.mode = Mode::Input;
+            self.iopin.set_mode(Mode::Input);
+        }
+    }
+
+    fn switch_output(&mut self) {
+        if self.mode != Mode::Output {
+            self.mode = Mode::Output;
+            self.iopin.set_mode(Mode::Output);
+        }
+    }
+}
+
+// Current rppal implementation does not support embedded_hal::gpio::v2 pins API.
+impl InputPin for OpenPin {
+    type Error = void::Void;
+
+    fn is_high(&self) -> Result<bool, Self::Error> {
+        Ok(self.iopin.is_high())
+    }
+
+    /// Is the input pin low?
+    fn is_low(&self) -> Result<bool, Self::Error> {
+        Ok(self.iopin.is_low())
+    }
+}
+
+// Current rppal implementation does not support embedded_hal::gpio::v2 pins API.
+impl OutputPin for OpenPin {
+    type Error = void::Void;
+
+    fn set_low(&mut self) -> Result<(), Self::Error> {
+        self.switch_output();
+        self.iopin.set_low();
+        Ok(())
+    }
+
+    fn set_high(&mut self) -> Result<(), Self::Error> {
+        self.iopin.set_high();
+        self.switch_input();
+        Ok(())
+    }
+}
+
+struct OutPin {
+    pin: rppal::gpio::OutputPin,
+}
+// Current rppal implementation does not support embedded_hal::gpio::v2 pins API.
+impl OutputPin for OutPin {
+    type Error = void::Void;
+
+    fn set_low(&mut self) -> Result<(), Self::Error> {
+        self.pin.set_low();
+        Ok(())
+    }
+
+    fn set_high(&mut self) -> Result<(), Self::Error> {
+        self.pin.set_high();
+        Ok(())
+    }
+}
+
+fn cli_matches() -> ArgMatches<'static> {
+    App::new("DHT tester")
+        .author("Rumato Estorsky")
+        .about("TM 163xx tests")
+        .arg(
+            Arg::with_name("clk")
+                .long("clk")
+                .value_name("PIN")
+                .help("CLK pin number")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("dio")
+                .long("dio")
+                .value_name("PIN")
+                .help("DIO pin number")
+                .required(true),
+        )
+        .get_matches()
+}
+
+struct Delayer;
+
+impl DelayUs<u16> for Delayer {
+    fn delay_us(&mut self, us: u16) {
+        spin_sleep::sleep(time::Duration::from_micros(us as u64));
+        //println!("D {}", us);
+    }
+}
+
+fn main() {
+    let matches = cli_matches();
+
+    // println!("MATCHES {:?}", matches);
+
+    let clk_num = matches
+        .value_of("clk")
+        .expect("Wrong CLK input")
+        .parse::<u8>()
+        .unwrap();
+    let dio_num = matches
+        .value_of("dio")
+        .expect("Wrong DIO input")
+        .parse::<u8>()
+        .unwrap();
+
+    println!("Initialized using CLK:{} DIO:{}", clk_num, dio_num);
+
+    let gpio = Gpio::new().expect("Can not init Gpio structure");
+
+    let clk = gpio
+        .get(clk_num)
+        .expect("Was not able to get CLK pin")
+        .into_output();
+
+    let dio = gpio
+        .get(dio_num)
+        .expect("Was not able to get CLK pin")
+        .into_io(Mode::Input);
+
+    let mut diopin = OpenPin::new(dio);
+    let mut clkpin = OutPin { pin: clk };
+    let mut delay = Delayer {};
+
+    let mut delayer = || spin_sleep::sleep(time::Duration::from_micros(350));
+
+    println!("Display starts");
+
+    let r = tm::tm_send_bytes(&mut diopin, &mut clkpin, &mut delayer, &[0x40]);
+    let r = tm::tm_send_bytes(
+        &mut diopin,
+        &mut clkpin,
+        &mut delayer,
+        &[0xc0_u8, 0x3f_u8, 0x3f_u8, 0x3f_u8, 0x3f_u8],
+    );
+    println!("Inited {:?}", r);
+
+    let r = tm::tm_send_bytes(
+        &mut diopin,
+        &mut clkpin,
+        &mut delayer,
+        &[tm::TM_COM_DISPLAY_ON | 0x0],
+    );
+    println!("Birght {:?}", r);
+
+    let mut nums: [u8; 5] = [tm::TM_COM_ADR | 0, 1, 2, 3, 4];
+    loop {
+        let mut bts: [u8; 5] = [0; 5];
+        bts[0] = nums[0];
+        for i in 1..nums.len() {
+            bts[i] = SEGMENTS[(nums[i] as usize % SEGMENTS.len())];
+        }
+
+        let r = tm::tm_send_bytes(&mut diopin, &mut clkpin, &mut delayer, &bts);
+        println!("Printed {:?}", r);
+
+        thread::sleep(time::Duration::from_secs(1));
+
+        for i in 1..nums.len() {
+            nums[i] = nums[i] + 1;
+        }
+    }
+}
+
+const SEGMENTS: [u8; 8] = [
+    tm::TM_SEGMENT_1,
+    tm::TM_SEGMENT_2,
+    tm::TM_SEGMENT_3,
+    tm::TM_SEGMENT_4,
+    tm::TM_SEGMENT_5,
+    tm::TM_SEGMENT_6,
+    tm::TM_SEGMENT_7,
+    tm::TM_SEGMENT_8,
+];
--- src/lib.rs
+++ src/lib.rs
@@ -1,88 +1,79 @@
 //#![deny(warnings)]
+//!
+//! More infromation about [7 segment displays](https://en.wikipedia.org/wiki/Seven-segment_display)
+//!
+//!
 #![no_std]
 use embedded_hal as hal;
 
 use hal::digital::v2::{InputPin, OutputPin};
 //use hal::blocking::delay::DelayUs;
 
-/// Data command mask
-pub const TM_COM_DATA: u8 = 0b01000000;
-/// Address command mask
-pub const TM_COM_ADR: u8 = 0b11000000;
-/// Display control command mask
-pub const TM_COM_DISPLAY: u8 = 0b10000000;
+/// Address adding mode (write to display)
+pub const TM_COM_DATA_ADDR_ADDING: u8 = 0b01000000;
+/// Data fix address mode (write to display)
+pub const TM_COM_DATA_ADDR_FIX: u8 = 0b01000100;
+/// Read key scan data
+pub const TM_COM_DATA_READ: u8 = 0b01000010;
 
-/// Bottom LED
+/// Register address command mask
+pub const TM_COM_ADR: u8 = 0b11000000;
+
+/// Display ON max brightness.
+/// Can be combined with masked bytes to adjust brightness level
+pub const TM_COM_DISPLAY_ON: u8 = 0b10001000;
+/// Display brightness mask
+pub const TM_DISPLAY_BRIGHTNESS_MASK: u8 = 0b00000111;
+// Display OFF
+pub const TM_COM_DISPLAY_OFF: u8 = 0b10000000;
+
+/// Segment A - top
 pub const TM_SEGMENT_1: u8 = 0b1;
-/// Bottom left LED
+/// Segment B - top right
 pub const TM_SEGMENT_2: u8 = 0b10;
-/// Top left LED
+/// Segment C - bottom right
 pub const TM_SEGMENT_3: u8 = 0b100;
-/// Top LED
+/// Segment D - bottom
 pub const TM_SEGMENT_4: u8 = 0b1000;
-/// Top right LED
+/// Segment E - bottom left
 pub const TM_SEGMENT_5: u8 = 0b10000;
-/// Bottom right LED
+/// Segment F - top left
 pub const TM_SEGMENT_6: u8 = 0b100000;
-/// Middle LED
+/// Segment G - middle
 pub const TM_SEGMENT_7: u8 = 0b1000000;
-/// Considering that 8th segment is a colon LED
+/// Segment DP (eight) - dot or colon
 pub const TM_SEGMENT_8: u8 = 0b10000000;
 
 #[derive(Debug)]
 pub enum TmError {
-    DIO,
-    DIO_UP(u8),
-    DIO_LOW(u8),
-    CLK,
-    ACK(u8),
-    SEND
+    Dio,
+    Ack(u8),
+    Clk,
+    Empty,
 }
 
 #[inline]
-fn tm_bus_dio_wait_level<DIO, D>(
+fn tm_bus_dio_wait_ack<DIO, D>(
     dio: &mut DIO,
     bus_delay: &mut D,
-    high: bool,
+    expect_high: bool,
     err: u8,
 ) -> Result<(), TmError>
 where
     DIO: InputPin + OutputPin,
     D: FnMut() -> (),
 {
-    for _ in 0..5 {
-        if high == dio.is_high().map_err(|_| TmError::DIO)? {
+    for _ in 0..10 {
+        if expect_high == dio.is_high().map_err(|_| TmError::Dio)? {
             return Ok(());
         }
         bus_delay();
     }
-    if high {
-        Err(TmError::DIO_UP(err))
-    } else {
-        Err(TmError::DIO_LOW(err))
-    }
+
+    Err(TmError::Ack(err))
 }
 
-#[inline]
-fn tm_bus_dio_high_ack<DIO, D>(dio: &mut DIO, bus_delay: &mut D, err: u8) -> Result<(), TmError>
-where
-    DIO: InputPin + OutputPin,
-    D: FnMut() -> (),
-{
-    dio.set_high().map_err(|_| TmError::DIO)?;
-    tm_bus_dio_wait_level(dio, bus_delay, true, err)
-}
-
-#[inline]
-fn tm_bus_dio_low_ack<DIO, D>(dio: &mut DIO, bus_delay: &mut D, err: u8) -> Result<(), TmError>
-where
-    DIO: InputPin + OutputPin,
-    D: FnMut() -> (),
-{
-    dio.set_low().map_err(|_| TmError::DIO)?;
-    tm_bus_dio_wait_level(dio, bus_delay, false, err)
-}
-
+/// Expecting to have initial state on bus CLK is UP and DIO is UP.
 #[inline]
 pub fn tm_bus_start<DIO, CLK, D>(
     dio: &mut DIO,
@@ -94,13 +85,7 @@ where
     CLK: OutputPin,
     D: FnMut() -> (),
 {
-    clk.set_high().map_err(|_| TmError::CLK)?;
-    tm_bus_dio_high_ack(dio, bus_delay, 1)?;
-    // bus_delay();
-    // tm_bus_dio_high_ack(dio, bus_delay, 1)?;
-    bus_delay();
-    tm_bus_dio_low_ack(dio, bus_delay, 2)?;
-    // dio.set_low().map_err(|_| TmError::DIO)?;
+    dio.set_low().map_err(|_| TmError::Dio)?;
     bus_delay();
     Ok(())
 }
@@ -116,20 +101,24 @@ where
     CLK: OutputPin,
     D: FnMut() -> (),
 {
-    clk.set_low().map_err(|_| TmError::CLK)?;
+    // clk.set_low().map_err(|_| TmError::Clk)?;
+    // bus_delay();
+
+    dio.set_low().map_err(|_| TmError::Dio)?;
+    // tm_bus_dio_low_ack(dio, bus_delay, 90)?;
     bus_delay();
 
-    //dio.set_low().map_err(|_| TmError::DIO)?;
-    tm_bus_dio_low_ack(dio, bus_delay, 90)?;
+    clk.set_high().map_err(|_| TmError::Clk)?;
     bus_delay();
 
-    clk.set_high().map_err(|_| TmError::CLK)?;
-    tm_bus_dio_high_ack(dio, bus_delay, 95)?;
+    dio.set_high().map_err(|_| TmError::Dio)?;
+    tm_bus_dio_wait_ack(dio, bus_delay, true, 255)?;
     bus_delay();
 
     Ok(())
 }
 
+/// Expecting to have delay after start sequence or previous send call
 #[inline]
 pub fn tm_bus_send<DIO, CLK, D>(
     dio: &mut DIO,
@@ -142,21 +131,20 @@ where
     CLK: OutputPin,
     D: FnMut() -> (),
 {
-    // Expecting to have delay after start command
     for _ in 0..8 {
-        clk.set_low().map_err(|_| TmError::CLK)?;
+        clk.set_low().map_err(|_| TmError::Clk)?;
+        bus_delay();
 
         let high = byte & 0b1 != 0;
-        if high {
-            dio.set_high().map_err(|_| TmError::DIO)?;
-        // tm_bus_dio_high_ack(dio, bus_delay, 100)?;
-        } else {
-            dio.set_low().map_err(|_| TmError::DIO)?;
-        }
-        tm_bus_dio_wait_level(dio, bus_delay, high, 42)?;
-
         byte = byte >> 1;
-        clk.set_high().map_err(|_| TmError::CLK)?;
+        if high {
+            dio.set_high().map_err(|_| TmError::Dio)?;
+        } else {
+            dio.set_low().map_err(|_| TmError::Dio)?;
+        }
+        bus_delay();
+
+        clk.set_high().map_err(|_| TmError::Clk)?;
         bus_delay();
     }
     Ok(())
@@ -168,41 +156,36 @@ pub fn tm_bus_ack<DIO, CLK, D>(
     dio: &mut DIO,
     clk: &mut CLK,
     bus_delay: &mut D,
+    err_code: u8,
 ) -> Result<(), TmError>
 where
     DIO: InputPin + OutputPin,
     CLK: OutputPin,
     D: FnMut() -> (),
 {
-    //dio.set_high().map_err(|_| TmError::DIO)?;
-    tm_bus_dio_high_ack(dio, bus_delay, 50).map_err(|_| TmError::ACK(0))?;
-
-    clk.set_low().map_err(|_| TmError::CLK)?;
-    // DIO to pull up - high state
-    bus_delay();
-    // let ack = tm_bus_dio_wait_level(dio, bus_delay, false, 50).is_ok();
-    // Ensure that high DIO was pulled down
-    tm_bus_dio_wait_level(dio, bus_delay, false, 51).map_err(|_| TmError::ACK(1))?;
-
-    // 9th cycle start
-    clk.set_high().map_err(|_| TmError::CLK)?;
+    // 8th cycle falling edge
+    dio.set_high().map_err(|_| TmError::Dio)?;
+    clk.set_low().map_err(|_| TmError::Clk)?;
     bus_delay();
 
-    // Ensure pin low at 9th cycle
-    tm_bus_dio_wait_level(dio, bus_delay, false, 52).map_err(|_| TmError::ACK(2))?;
+    // Ensure that DIO was pulled down at 8th cycle falling edge
+    tm_bus_dio_wait_ack(dio, bus_delay, false, err_code + 1)?;
 
-    clk.set_low().map_err(|_| TmError::CLK)?;
+    // 9th cycle rising edge
+    clk.set_high().map_err(|_| TmError::Clk)?;
     bus_delay();
 
-    // Ensure pin was released and now it is up
-    tm_bus_dio_wait_level(dio, bus_delay, true, 53).map_err(|_| TmError::ACK(3))?;
+    // Ensure DIO still low at 9th cycle rising edge
+    tm_bus_dio_wait_ack(dio, bus_delay, false, err_code + 2)?;
+
+    // 9th cycle falling edge
+    clk.set_low().map_err(|_| TmError::Clk)?;
+    bus_delay();
+
+    // Ensure DIO was released and now it is up
+    tm_bus_dio_wait_ack(dio, bus_delay, true, err_code + 3)?;
 
     Ok(())
-    // if ack {
-    //     Ok(())
-    // } else {
-    //     Err(TmError::ACK)
-    // }
 }
 
 #[inline]
@@ -211,6 +194,7 @@ pub fn tm_bus_send_byte<DIO, CLK, D>(
     clk: &mut CLK,
     bus_delay: &mut D,
     byte: u8,
+    err_code: u8,
 ) -> Result<(), TmError>
 where
     DIO: InputPin + OutputPin,
@@ -218,7 +202,7 @@ where
     D: FnMut() -> (),
 {
     tm_bus_send(dio, clk, bus_delay, byte)?;
-    tm_bus_ack(dio, clk, bus_delay)
+    tm_bus_ack(dio, clk, bus_delay, err_code)
 }
 
 ///
@@ -238,12 +222,14 @@ where
 {
     tm_bus_start(dio, clk, bus_delay)?;
 
-    let mut send = Err(TmError::SEND);
+    let mut send = Err(TmError::Empty);
+    let mut iter = 10;
     for bt in bytes {
-        send = tm_bus_send_byte(dio, clk, bus_delay, bt.clone());
+        send = tm_bus_send_byte(dio, clk, bus_delay, bt.clone(), iter);
         if send.is_err() {
             break;
         }
+        iter += 10;
     }
 
     let stop = tm_bus_stop(dio, clk, bus_delay);
