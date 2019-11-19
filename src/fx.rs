869530e255b690a8e237de035c877facd58d0070
Rumato Estorsky <rumatoest@users.noreply.github.com>
2019-11-19 10:05:04 +0300

    Animations core prototype

--- src/fx.rs
+++ src/fx.rs
@@ -8,12 +8,10 @@ const CIRCLE: [u8; 6] = [SEG_1, SEG_2, SEG_3, SEG_4, SEG_5, SEG_6];
 /// Simple iterator-like animation functionality.
 ///
 pub trait Animate<R> {
-    ///
-    /// Return next animation data (value), that you should send to MCU.
+    /// Return next animation payload (value), that you should send to MCU somehow.
     /// Delays and other stuff should be handled in your code.
     ///
     /// If animation completed returns `None` for infinite animations always returns something.
-    ///
     fn next(&mut self) -> Option<R>;
 }
 
@@ -70,22 +68,22 @@ pub enum SlideType {
 }
 
 #[derive(Debug)]
-pub struct Slide {
+pub struct Slider {
     tp: SlideType,
     count: u8,
     result_len: u8,
     word: Vec<u8>,
 }
 
-impl Slide {
-    pub fn new(slide_type: SlideType, displays_count: u8, bytes_to_slide: &[u8]) -> Slide {
+impl Slider {
+    pub fn new(slide_type: SlideType, displays_count: u8, bytes_to_slide: &[u8]) -> Slider {
         let mut word = Vec::<u8>::new();
 
         for i in 0..bytes_to_slide.len() {
             word.push(bytes_to_slide[i]);
         }
 
-        Slide {
+        Slider {
             tp: slide_type,
             count: 0,
             result_len: displays_count,
@@ -94,7 +92,7 @@ impl Slide {
     }
 }
 
-impl Animate<Vec<u8>> for Slide {
+impl Animate<Vec<u8>> for Slider {
     fn next(&mut self) -> Option<Vec<u8>> {
         if self.count == 255 {
             // Do not support long words
@@ -124,12 +122,6 @@ impl Animate<Vec<u8>> for Slide {
             }
         }
 
-        let len_in = if off_in > self.result_len as usize {
-            0
-        } else {
-            self.result_len as usize - off_in
-        };
-
         if off_in >= self.word.len() {
             // Stop if last char already out of screen
             if let SlideType::StopAfterLastChar = self.tp {
@@ -137,9 +129,13 @@ impl Animate<Vec<u8>> for Slide {
             }
         }
 
-        if out.len() < self.result_len as usize && (off_in as usize) < self.word.len() {
+        if (off_in as usize) < self.word.len() {
             for i in 0..self.word.len() {
-                if i < off_in || i >= off_in + len_in {
+                if out.len() >= self.result_len as usize {
+                    break;
+                }
+                if i < off_in {
+                    // Skip first chars
                     continue;
                 }
                 out.push(self.word[i]);
@@ -170,14 +166,22 @@ mod tests {
     use super::*;
 
     fn animate_next<R>(ani: &mut dyn Animate<R>, steps: usize) -> Option<R> {
-        if steps <= 0 {
-            panic!("Wrong steps argument value = 0");
-        }
+        // if steps <= 0 {
+        //     panic!("Wrong steps argument value = 0");
+        // }
         let mut last = None;
         for _ in 0..steps {
             last = ani.next();
         }
         last
+    }
+
+    fn animate_none<R>(ani: &mut dyn Animate<R>, steps: usize) {
+        for s in 0..steps {
+            if let Some(_) = ani.next() {
+                assert!(false, "Resultis not None at step {}", s);
+            }
+        }
     }
 
     #[test]
@@ -203,8 +207,52 @@ mod tests {
             CHAR_0, CHAR_1, CHAR_2, CHAR_3, CHAR_4, CHAR_5, CHAR_6, CHAR_7, CHAR_8, CHAR_9,
         ];
 
-        let mut sstop = Slide::new(SlideType::StopAtFirstChar, 5, &init);
-        assert_eq!(sstop.next().unwrap(), [0; 5]);
-        assert_eq!(sstop.next().unwrap(), [0, 0, 0, 0, CHAR_0]);
+        let mut sstop = Slider::new(SlideType::StopAtFirstChar, 5, &init);
+        assert_eq!(animate_next(&mut sstop, 1).unwrap(), [0; 5]);
+        assert_eq!(animate_next(&mut sstop, 1).unwrap(), [0, 0, 0, 0, CHAR_0]);
+        assert_eq!(
+            animate_next(&mut sstop, 4).unwrap(),
+            [CHAR_0, CHAR_1, CHAR_2, CHAR_3, CHAR_4]
+        );
+        animate_none(&mut sstop, 15);
+
+        let mut sstopaft = Slider::new(SlideType::StopAfterLastChar, 5, &init);
+        assert_eq!(animate_next(&mut sstopaft, 1).unwrap(), [0; 5]);
+        assert_eq!(
+            animate_next(&mut sstopaft, 1).unwrap(),
+            [0, 0, 0, 0, CHAR_0]
+        );
+        assert_eq!(
+            animate_next(&mut sstopaft, 4).unwrap(),
+            [CHAR_0, CHAR_1, CHAR_2, CHAR_3, CHAR_4]
+        );
+        assert_eq!(
+            animate_next(&mut sstopaft, 3).unwrap(),
+            [CHAR_3, CHAR_4, CHAR_5, CHAR_6, CHAR_7]
+        );
+        assert_eq!(
+            animate_next(&mut sstopaft, 2).unwrap(),
+            [CHAR_5, CHAR_6, CHAR_7, CHAR_8, CHAR_9]
+        );
+        assert_eq!(
+            animate_next(&mut sstopaft, 4).unwrap(),
+            [CHAR_9, 0, 0, 0, 0]
+        );
+        animate_none(&mut sstop, 15);
+
+        let mut cycle = Slider::new(SlideType::Cycle, 5, &init);
+        assert_eq!(animate_next(&mut cycle, 1).unwrap(), [0; 5]);
+        assert_eq!(
+            animate_next(&mut cycle, 3 * (init.len() + 5)).unwrap(),
+            [0; 5]
+        );
+        assert_eq!(
+            animate_next(&mut cycle, 5).unwrap(),
+            [CHAR_0, CHAR_1, CHAR_2, CHAR_3, CHAR_4]
+        );
+        assert_eq!(
+            animate_next(&mut cycle, 5).unwrap(),
+            [CHAR_5, CHAR_6, CHAR_7, CHAR_8, CHAR_9]
+        );
     }
 }
